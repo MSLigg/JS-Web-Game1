bd0e29484a7b0aba6f4da334b91e08d7fde4ab5b
Mylo <mslx92@yahoo.com>
2022-05-08 21:09:50 -0400

    think I finished it

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+let myFunc = document.createElement('img')
+myFunc.src = url
+myFunc.style.position = 'fixed'
+myFunc.style.left = left + 'px'
+myFunc.style.bottom = bottom + 'px'
+document.body.append(myFunc)
+return myFunc
+}
+
+function newItem(url, left, bottom){
+    let myFunc = newImage(url, left, bottom)
+
+    myFunc.addEventListener('dblclick', function(){
+        myFunc.remove()
+     })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 265, 185)
+newItem('assets/staff.png', 600, 100)
